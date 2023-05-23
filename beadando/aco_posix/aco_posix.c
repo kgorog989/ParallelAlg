dbbe4576202f69da88ec9dda74590935b5ae93b5
kgorog989 <kgorog989@gmail.com>
2023-05-23 19:24:10 +0200

    fixing aco posic implementation

--- beadando/aco_posix/aco_posix.c
+++ beadando/aco_posix/aco_posix.c
@@ -12,11 +12,16 @@
 #define RHO 0.5
 #define Q 100.0
 
-const int N_THREADS = 10;
+const int N_THREADS = 50;
+int ant_count = 0;
+
+pthread_mutex_t ant_lengths_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t ant_tours_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t rand_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t ant_count_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct Task
 {
-    int ant_id;
     int num_ants;
     int num_cities;
     double *city_distances;
@@ -29,17 +34,17 @@ void init_distance_matrix(const char *distances, Task *task);
 void init_pheromones(Task *task);
 void init_ants(Task *task);
 void *construct_ant_tour(void *arg);
-void update_pheromones(int num_cities, int num_ants, double (*pheromones)[num_cities], int (*ant_tours)[num_cities], double *ant_lengths);
-void find_best_tour(int num_cities, int num_ants, int (*ant_tours)[num_cities], double *ant_lengths, int *best_tour, double *best_length);
+void update_pheromones(Task *task);
+void find_best_tour(Task *task, int *best_tour, double *best_length);
 
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
     pthread_t threads[N_THREADS];
-    int num_iterations = 1;
+    int num_iterations = 100;
     Task task;
     task.num_ants = N_THREADS;
-    task.num_cities = 7;
+    task.num_cities = 312;
     task.city_distances = (double *)malloc(task.num_cities * task.num_cities * sizeof(double));
     task.pheromones = (double *)malloc(task.num_cities * task.num_cities * sizeof(double));
     task.ant_tours = (int *)malloc(task.num_ants * task.num_cities * sizeof(int));
@@ -47,51 +52,41 @@ int main(int argc, char *argv[])
     int best_tour[task.num_cities];
     double best_length = INFINITY;
 
-    init_distance_matrix("data/sh07.txt", &task);
+    init_distance_matrix("data/usca312.txt", &task);
     init_pheromones(&task);
     init_ants(&task);
 
     for (int q = 0; q < num_iterations; q++)
     {
+        ant_count = 0;
         printf(":: Start threads ...\n");
         for (int j = 0; j < N_THREADS; j++)
         {
-            task.ant_id = j;
-            pthread_create(&threads[j], NULL, construct_ant_tour, (void*)&task);
-            printf("Thread %d created\n", j);
+            pthread_create(&threads[j], NULL, construct_ant_tour, (void *)&task);
+            printf("Thread %d created in iteration %d\n", j, q + 1);
         }
 
-        // update_pheromones(num_cities, num_ants, pheromones, ant_tours, ant_lengths);
-        // find_best_tour(num_cities, num_ants, ant_tours, ant_lengths, best_tour, &best_length);
+        printf(":: Join threads ...\n");
+        for (int i = 0; i < N_THREADS; ++i)
+        {
+            pthread_join(threads[i], NULL);
+        }
+
+        update_pheromones(&task);
+        find_best_tour(&task, best_tour, &best_length);
     }
 
-    for (int i = 0; i < task.num_ants; i++)
+    printf("\nBest tour: ");
+    for (int i = 0; i < task.num_cities; i++)
     {
-        for (int j = 0; j < task.num_cities; j++)
-        {
-            int offset = i * task.num_cities + j;
-            printf("%d ", task.ant_tours[offset]);
-        }
-        printf("\n");
+        printf("%d ", best_tour[i]);
     }
-    /*
-        printf("\nBest tour: ");
-        for (int i = 0; i < task.num_cities; i++)
-        {
-            printf("%d ", best_tour[i]);
-        }
-        printf("\nBest tour length: %lf\n", best_length);
-    */
+    printf("\nBest tour length: %lf\n", best_length);
+
     free(task.city_distances);
     free(task.pheromones);
     free(task.ant_tours);
     free(task.ant_lengths);
-
-    printf(":: Join threads ...\n");
-    for (int i = 0; i < N_THREADS; ++i)
-    {
-        pthread_join(threads[i], NULL);
-    }
 
     return 0;
 }
@@ -148,6 +143,12 @@ void init_ants(Task *task)
 void *construct_ant_tour(void *arg)
 {
     Task *task = (Task *)arg;
+    int ant_id;
+
+    pthread_mutex_lock(&ant_count_mutex);
+    ant_id = ant_count;
+    ant_count++;
+    pthread_mutex_unlock(&ant_count_mutex);
 
     double total_probabilities, random_num, probability_sum, probability_to_move;
     int current_city, next_city, next_city_found;
@@ -164,14 +165,16 @@ void *construct_ant_tour(void *arg)
     {
         visited_cities[j] = 0;
     }
-    offset = task->ant_id * task->num_cities + 0;
+    offset = ant_id * task->num_cities + 0;
     visited_cities[task->ant_tours[offset]] = 1;
     current_city = task->ant_tours[offset];
 
     for (int k = 1; k < task->num_cities; k++)
     {
         total_probabilities = 0;
+        pthread_mutex_lock(&rand_mutex);
         random_num = (double)rand() / RAND_MAX;
+        pthread_mutex_unlock(&rand_mutex);
         probability_sum = 0;
         for (int l = 0; l < task->num_cities; l++)
         {
@@ -201,7 +204,7 @@ void *construct_ant_tour(void *arg)
 
         if (next_city_found == 0)
         {
-            for (int n = task->num_cities; n >= 0; n--)
+            for (int n = task->num_cities - 1; n >= 0; n--)
             {
                 if (visited_cities[n] == 0)
                 {
@@ -211,69 +214,88 @@ void *construct_ant_tour(void *arg)
             }
         }
 
-        offset = task->ant_id * task->num_cities + k;
+        offset = ant_id * task->num_cities + k;
+        pthread_mutex_lock(&ant_tours_mutex);
         task->ant_tours[offset] = next_city;
+        pthread_mutex_unlock(&ant_tours_mutex);
 
         offset = current_city * task->num_cities + next_city;
-        task->ant_lengths[task->ant_id] += task->city_distances[offset];
+        pthread_mutex_lock(&ant_lengths_mutex);
+        task->ant_lengths[ant_id] += task->city_distances[offset];
+        pthread_mutex_unlock(&ant_lengths_mutex);
 
         visited_cities[next_city] = 1;
         current_city = next_city;
     }
 
     // Adding the distance between the starting and ending city to the length of the tour
-    offset = task->ant_id * task->num_cities + (task->num_cities - 1);
+    offset = ant_id * task->num_cities + (task->num_cities - 1);
     int last_num = task->ant_tours[offset];
 
-    offset = task->ant_id * task->num_cities + 0;
+    offset = ant_id * task->num_cities + 0;
     int first_num = task->ant_tours[offset];
 
     offset = last_num * task->num_cities + first_num;
-    task->ant_lengths[task->ant_id] += task->city_distances[offset];
+    pthread_mutex_lock(&ant_lengths_mutex);
+    task->ant_lengths[ant_id] += task->city_distances[offset];
+    pthread_mutex_unlock(&ant_lengths_mutex);
 
     free(visited_cities);
 
     return NULL;
 }
 
-void update_pheromones(int num_cities, int num_ants, double (*pheromones)[num_cities], int (*ant_tours)[num_cities], double *ant_lengths)
+void update_pheromones(Task *task)
 {
     int city1, city2;
+    int offset;
     // Evaporating pheromones
-    for (int i = 0; i < num_cities; i++)
+    for (int i = 0; i < task->num_cities; i++)
     {
-        for (int j = 0; j < num_cities; j++)
+        for (int j = 0; j < task->num_cities; j++)
         {
-            pheromones[i][j] *= (1.0 - RHO);
+            offset = i * task->num_cities + j;
+            task->pheromones[offset] *= (1.0 - RHO);
         }
     }
     // Depositing pheromones
-    for (int i = 0; i < num_ants; i++)
+    for (int i = 0; i < task->num_ants; i++)
     {
-        for (int j = 0; j < num_cities - 1; j++)
+        for (int j = 0; j < task->num_cities - 1; j++)
         {
-            city1 = ant_tours[i][j];
-            city2 = ant_tours[i][j + 1];
-            pheromones[city1][city2] += Q / ant_lengths[i];
-            pheromones[city2][city1] += Q / ant_lengths[i];
+
+            offset = i * task->num_cities + j;
+            city1 = task->ant_tours[offset];
+            offset++;
+            city2 = task->ant_tours[offset];
+            offset = city1 * task->num_cities + city2;
+            task->pheromones[offset] += Q / task->ant_lengths[i];
+            offset = city2 * task->num_cities + city1;
+            task->pheromones[offset] += Q / task->ant_lengths[i];
         }
-        city1 = ant_tours[i][num_cities - 1];
-        city2 = ant_tours[i][0];
-        pheromones[city1][city2] += Q / ant_lengths[i];
-        pheromones[city2][city1] += Q / ant_lengths[i];
+        offset = i * task->num_cities + task->num_cities - 1;
+        city1 = task->ant_tours[offset];
+        offset = i * task->num_cities + 0;
+        city2 = task->ant_tours[offset];
+        offset = city1 * task->num_cities + city2;
+        task->pheromones[offset] += Q / task->ant_lengths[i];
+        offset = city2 * task->num_cities + city1;
+        task->pheromones[offset] += Q / task->ant_lengths[i];
     }
 }
 
-void find_best_tour(int num_cities, int num_ants, int (*ant_tours)[num_cities], double *ant_lengths, int *best_tour, double *best_length)
+void find_best_tour(Task *task, int *best_tour, double *best_length)
 {
-    for (int i = 0; i < num_ants; i++)
+    int offset;
+    for (int i = 0; i < task->num_ants; i++)
     {
-        if (ant_lengths[i] < *best_length)
+        if (task->ant_lengths[i] < *best_length)
         {
-            *best_length = ant_lengths[i];
-            for (int j = 0; j < num_cities; j++)
+            *best_length = task->ant_lengths[i];
+            for (int j = 0; j < task->num_cities; j++)
             {
-                best_tour[j] = ant_tours[i][j];
+                offset = i * task->num_cities + j;
+                best_tour[j] = task->ant_tours[offset];
             }
         }
     }
